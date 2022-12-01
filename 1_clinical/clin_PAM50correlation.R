4b001fe0b27424ee81d9f2a7a2cbaa90227cbe02
Lennart <lennart.hohmann@med.lu.se>
2022-12-01 16:15:27 +0100

    adapted correlation script

--- 1_clinical/clin_PAM50correlation.R
+++ 1_clinical/clin_PAM50correlation.R
@@ -37,8 +37,6 @@ plot.list <- list()
 # load data
 anno <- loadRData("./data/SCANB/1_clinical/raw/SampleSet_WhoAmI_PAM50_n6233_Rel4_with_PAM50correlations_NoUnclassified.RData")
 
-group.colors <- c(meanLumA = "#2176d5", meanLumB = "#34c6eb", meanHer2 ="#d334eb", meanBasal ="#c41b0e", meanNormal="#64c25d")
-
 pam50.subtypes <- unique(anno$NCN_PAM50)
 
 clinical.groups <- setNames(as.data.frame(rbind(
@@ -83,10 +81,131 @@ for (i in 1:nrow(clinical.groups)) {
               axis.text.y = element_text(size = 20),
               axis.title.y = element_text(size = 25),
               legend.position = "none") +
-        scale_fill_manual(values=group.colors)))
+        scale_fill_manual(values=group.colors <- c(meanLumA = "#2176d5", meanLumB = "#34c6eb", meanHer2 ="#d334eb", meanBasal ="#c41b0e", meanNormal="#64c25d"))))
   }
 }
 
+#######################################################################
+# Crosstable with 2nd best PAM50 match for all samples
+#######################################################################
+
+for (i in 1:nrow(clinical.groups)) { 
+  # select subgroup data
+  clin.group <- row.names(clinical.groups)[i] #i
+  anno <- anno[which(anno$majorityNearestClass!=anno$majoritySecondBestClass),]
+  if (clin.group == "All") {
+    sg.data <- anno
+  } else {
+    sg.data <- anno[which(
+      anno$HER2 == clinical.groups[clin.group ,"HER2"] & 
+        anno$ER == clinical.groups[clin.group ,"ER"]),]
+  }
+  
+  # get cm
+  confusion_matrix <- as.data.frame(table(
+    sg.data$majorityNearestClass,sg.data$majoritySecondBestClass,dnn=c("majorityNearestClass","majoritySecondBestClass")))
+  
+  # plot
+  plot.list <- append(plot.list,list(
+    ggplot(data = confusion_matrix,
+           mapping = aes(x = majorityNearestClass,
+                         y = majoritySecondBestClass)) +
+      scale_x_discrete(limits = rev(levels(confusion_matrix$majorityNearestClass))) +
+      geom_tile(aes(fill = Freq)) +
+      geom_text(aes(label = sprintf("%1.0f", Freq)), size=10, vjust = 1) +
+      scale_fill_gradient(low = "#fee6ce",
+                          high = "#e6550d",
+                          trans = "log") +
+      ggtitle(paste("Second-best PAM50 match - clin: ",clin.group,sep="")) +
+      xlab("majorityNearestClass") +
+      ylab("majoritySecondBestClass") +
+      theme(plot.title = element_text(size = 25),
+            axis.text.x = element_text(size = 20),
+            axis.title.x = element_text(size = 25),
+            axis.text.y = element_text(size = 20),
+            axis.title.y = element_text(size = 25), 
+            legend.position = "none")))
+}
+
+
+#######################################################################
+# Distinctiveness: Boxplot the difference between the best and second 
+# best correlation matches
+#######################################################################
+
+for (i in 1:nrow(clinical.groups)) { 
+  # select subgroup data
+  clin.group <- row.names(clinical.groups)[i] 
+  # filter out samples where best=2nd best and calc diff.
+  anno <- anno[which(
+    anno$majorityNearestClass!=anno$majoritySecondBestClass),] %>% 
+    rowwise() %>% mutate(Diff = abs(get(paste("mean",majorityNearestClass,sep=""))) - abs(get(paste("mean",majoritySecondBestClass,sep=""))))
+  
+  if (clin.group == "All") {
+    sg.data <- anno
+  } else {
+    sg.data <- anno[which(
+      anno$HER2 == clinical.groups[clin.group ,"HER2"] & 
+        anno$ER == clinical.groups[clin.group ,"ER"]),]
+  }
+  
+  # plot
+  plot.list <- append(plot.list,list(
+    ggplot(sg.data) + 
+    geom_boxplot(aes(x=majorityNearestClass, y=Diff,fill=as.factor(majorityNearestClass)),alpha=0.7, size=1.5, outlier.size = 5) +
+    xlab("PAM50 Class") +
+    ylab("Correlation diff (1st-2nd)") +
+    ggtitle(paste("PAM50 class distinctiveness - clin: ",clin.group,sep="")) +
+    theme(plot.title = element_text(size = 25),
+          axis.text.x = element_text(size = 20),
+          axis.title.x = element_text(size = 25),
+          axis.text.y = element_text(size = 20),
+          axis.title.y = element_text(size = 25),
+          legend.position = "none") +
+    scale_fill_manual(values= c(LumA = "#2176d5", LumB = "#34c6eb", Her2 ="#d334eb", Basal ="#c41b0e", Normal="#64c25d"))))
+}
+
+#######################################################################
+# PAM50 Class specific distinctiveness: difference between the best and second best correlation matches
+#######################################################################
+
+for (i in 1:nrow(clinical.groups)) { 
+  # select subgroup data
+  clin.group <- row.names(clinical.groups)[i] 
+  # filter out samples where best=2nd best and calc diff.
+  anno <- anno[which(
+    anno$majorityNearestClass!=anno$majoritySecondBestClass),] %>% 
+    rowwise() %>% mutate(Diff = abs(get(paste("mean",majorityNearestClass,sep=""))) - abs(get(paste("mean",majoritySecondBestClass,sep=""))))
+  
+  if (clin.group == "All") {
+    sg.data <- anno
+  } else {
+    sg.data <- anno[which(
+      anno$HER2 == clinical.groups[clin.group ,"HER2"] & 
+        anno$ER == clinical.groups[clin.group ,"ER"]),]
+  }
+  
+  for (j in 1:length(pam50.subtypes)) {
+    # select data 
+    pam50.type <- pam50.subtypes[j]
+    pam50.data <- sg.data[which(sg.data$NCN_PAM50 == pam50.type),]
+    
+    # plot
+    plot.list <- append(plot.list,list(
+      ggplot(pam50.data) + 
+        geom_boxplot(aes(x=majoritySecondBestClass, y=Diff,fill=as.factor(majoritySecondBestClass)),alpha=0.7, size=1.5, outlier.size = 5) +
+        xlab("majoritySecondBestClass") +
+        ylab("Correlation diff (1st-2nd)") +
+        ggtitle(paste("PAM50 class-specific distinctiveness - clin: ",clin.group,"; pam50: ",pam50.type,sep="")) +
+        theme(plot.title = element_text(size = 25),
+              axis.text.x = element_text(size = 20),
+              axis.title.x = element_text(size = 25),
+              axis.text.y = element_text(size = 20),
+              axis.title.y = element_text(size = 25),
+              legend.position = "none") +
+        scale_fill_manual(values= c(LumA = "#2176d5", LumB = "#34c6eb", Her2 ="#d334eb", Basal ="#c41b0e", Normal="#64c25d"))))
+  }
+}
 
 #plot
 pdf(file = paste(output.path,"PAM50_correlation.pdf", sep=""), 
@@ -98,127 +217,11 @@ for (i in 1:length(plot.list)) {
 
 dev.off()
 
-
 # SO FAR
-#######################################################################
-# Crosstable with 2nd best PAM50 match for all samples
-#######################################################################
-
-# select subgroup data
-anno <- clin.rel4 %>% 
-    filter(Follow.up.cohort == TRUE) %>% 
-    filter(ER=="Positive" & HER2=="Negative") 
-
-data <- pam50ann_correlations %>% 
-    filter(Assay %in% anno$GEX.assay) %>% 
-    filter(majorityClass != "unclassified") %>% 
-    filter(majorityNearest != majoritySecondBest) %>% 
-    distinct() #S003571.l.r.m.c.lib.g.k2.a.t occurs 2 times for some reason, the data is identical (duplicated) so I exclude one
-
-confusion_matrix <- as.data.frame(table(data$majorityNearest,data$majoritySecondBest,dnn=c("majorityNearest","majoritySecondBest")))
-
-plot.list <- append(plot.list,list(
-    ggplot(data = confusion_matrix,
-       mapping = aes(x = majorityNearest,
-                     y = majoritySecondBest)) +
-    scale_x_discrete(limits = rev(levels(confusion_matrix$majorityNearest))) +
-    geom_tile(aes(fill = Freq)) +
-    geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
-    scale_fill_gradient(low = "#fee6ce",
-                        high = "#e6550d",
-                        trans = "log") +
-    ggtitle("2nd best PAM50 match in ERpHER2n samples") +
-    xlab("PAM50 Class") +
-    ylab("2nd best PAM50 match") +
-    theme(plot.title = element_text(size = 25),
-          axis.text.x = element_text(size = 20),
-          axis.title.x = element_text(size = 25),
-          axis.text.y = element_text(size = 20),
-          axis.title.y = element_text(size = 25),
-          legend.position = "none")))
-
-# small table for her2e
-her2e_matrix <- confusion_matrix %>% 
-    filter(majorityNearest == "Her2") %>% 
-    droplevels() %>% 
-    dplyr::select(-c(majorityNearest)) %>% 
-    column_to_rownames(var="majoritySecondBest")
-her2e_matrix <- as.data.frame(t(her2e_matrix)) %>% dplyr::select(-c(2))
-write.xlsx(her2e_matrix, file = paste(data.path,"PAM50_HER2E_2ndbest.xlsx",sep=""),overwrite = TRUE)
 
 #######################################################################
-# Distinctiveness: Boxplot the difference between the best and second 
-# best correlation matches
+# plot correlations
 #######################################################################
-
-# select subgroup data
-anno <- clin.rel4 %>% 
-    filter(Follow.up.cohort == TRUE) %>% 
-    filter(ER=="Positive" & HER2=="Negative") 
-
-data <- pam50ann_correlations %>% 
-    filter(Assay %in% anno$GEX.assay) %>% 
-    filter(majorityClass != "unclassified") %>% 
-    filter(majorityNearest != majoritySecondBest) %>% 
-    distinct() 
-
-# dataframe best, diff to 2nd best
-data <- data %>% rowwise() %>% mutate(Diff = abs(get(paste("mean",majorityNearest,sep=""))) - abs(get(paste("mean",majoritySecondBest,sep=""))))
-
-# plot
-plot.list <- append(plot.list,list(
-    ggplot(data) + 
-    geom_boxplot(aes(x=majorityNearest, y=Diff,fill=as.factor(majorityNearest)),alpha=0.7, size=1.5, outlier.size = 5) +
-    xlab("PAM50 Class") +
-    ylab("Correlation diff (1st-2nd)") +
-    ggtitle("PAM50 class distinctiveness in ERpHER2n samples") +
-    theme(plot.title = element_text(size = 25),
-          axis.text.x = element_text(size = 20),
-          axis.title.x = element_text(size = 25),
-          axis.text.y = element_text(size = 20),
-          axis.title.y = element_text(size = 25),
-          legend.position = "none") +
-    scale_fill_manual(values=c(LumA = "#2176d5", LumB = "#34c6eb", Her2 ="#d334eb", Basal ="#c41b0e", Normal="#64c25d"))))
-
-#######################################################################
-# HER2E specific distinctiveness: Boxplot for the her2e samples the difference between the best (her2e) and second best correlation matches
-# so three groups in boxplot: basal, luma, lumb, normal
-#######################################################################
-
-# select subgroup data
-anno <- clin.rel4 %>% 
-    filter(Follow.up.cohort == TRUE) %>% 
-    filter(ER=="Positive" & HER2=="Negative") %>% 
-    filter(NCN.PAM50 == "Her2") 
-
-data <- pam50ann_correlations %>% 
-    filter(Assay %in% anno$GEX.assay) %>% 
-    filter(majorityClass != "unclassified") %>% 
-    filter(majorityNearest != majoritySecondBest) %>% 
-    distinct() 
-
-#table(data$majoritySecondBest)
-# dataframe best, diff to 2nd best
-data <- data %>% rowwise() %>% mutate(HER2Diff = abs(get(paste("mean",majorityNearest,sep=""))) - abs(get(paste("mean",majoritySecondBest,sep=""))))
-
-# plot
-plot.list <- append(plot.list,list(
-    ggplot(data) + 
-    geom_boxplot(aes(x=majoritySecondBest, y=HER2Diff,fill=as.factor(majoritySecondBest)),alpha=0.7, size=1.5, outlier.size = 5) +
-    xlab("Second-best PAM50 Class") +
-    ylab("Correlation diff (1st-2nd)") +
-    ggtitle("PAM50-HER2E distinctiveness in ERpHER2nHER2E") +
-    theme(plot.title = element_text(size = 25),
-          axis.text.x = element_text(size = 20),
-          axis.title.x = element_text(size = 25),
-          axis.text.y = element_text(size = 20),
-          axis.title.y = element_text(size = 25),
-          legend.position = "none") +
-    scale_fill_manual(values=c(LumA = "#2176d5", LumB = "#34c6eb", Her2 ="#d334eb", Basal ="#c41b0e", Normal="#64c25d"))))
-
-# further investigate the luma,lumb,basal samples
-# creating modified dataframe
-data_mod <- data %>% dplyr::select(c(majoritySecondBest,HER2Diff,meanHer2))
 
 # all samples
 plot.list <- append(plot.list,list(
@@ -296,12 +299,4 @@ plot.list <- append(plot.list,list(
     scale_x_continuous(limits = c(0.2, 0.8),breaks=seq(0, 0.8, 0.1)) +
     scale_y_continuous(limits = c(0,0.6),breaks=seq(0, 0.6, 0.1))))
 
-# save plots
-pdf(file = paste(output.path,cohort,"_PAM50correlation.pdf", sep=""), 
-    onefile = TRUE, width = 21, height = 14.8) 
 
-for (i in 1:length(plot.list)) {
-    print(plot.list[[i]])
-}
-
-dev.off()
